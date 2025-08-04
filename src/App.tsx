1cd9d11b60e4e3d7b919efd17b462b285094f35f
Pham Van Phuc <phucpham.1803@gmail.com>
2025-08-04 22:07:18 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-10 text-2xl text-center">
+      Nếu nền này màu đỏ là Tailwind đã hoạt động 🟥
+    </div>
+  );
+}
+
+export default App;
+
+
